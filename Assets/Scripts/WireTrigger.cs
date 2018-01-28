14bbe18ca7dfe9b38c7fb566725ee34992d51842
Livio De La Cruz <livio@livio.me>
2018-01-28 15:56:46 -0800

    wires spark when cut

--- Assets/Scripts/WireTrigger.cs
+++ Assets/Scripts/WireTrigger.cs
@@ -3,10 +3,12 @@
 public class WireTrigger : InteractiveObject
 {
 	public Rigidbody wireEnd;
+	public ParticleSystem ps;
 
 	public override void Interact()
 	{
 		wireEnd.isKinematic = false;
+		ps.Play();
 		gameObject.SetActive(false);
 	}
 }
