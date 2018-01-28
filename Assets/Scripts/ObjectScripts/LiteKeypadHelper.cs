9d4456e180f98968b274386d169982dd1ec7ca94
Henry Banks <hbanks410@gmail.com>
2018-01-28 11:24:29 -0800

    door keypad stuff

---
+++ Assets/Scripts/ObjectScripts/LiteKeypadHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Lighter version of VaultPasswordGenerator.cs
+public class LiteKeypadHelper : MonoBehaviour {
+
+    List<char> symbols;
+
+    public LockedDoor doorRef;
+    //The keypad, for key generation.
+    public GameObject keypadRef;
+
+    //File contains all characters that will be used within the password.
+    public TextAsset symbolList;
+
+    private void Awake()
+    {
+        if (keypadRef == null)
+            keypadRef = gameObject;
+
+        symbols = new List<char>();
+
+        //Populate list
+        foreach (char c in symbolList.text)
+        {
+            symbols.Add(c);
+        }
+
+        Keypad[] keys = keypadRef.GetComponentsInChildren<Keypad>();
+        Text[] texts = keypadRef.GetComponentsInChildren<Text>();
+
+        //Make keys
+        for (int i = 0; i < symbols.Count; i++)
+        {
+            keys[i].symbol = symbols[i];
+            keys[i].doorRef = doorRef;
+            texts[i].text = char.ToString(symbols[i]);
+        }
+    }
+
+    private void Update()
+    {
+        if (keypadRef.GetComponentInChildren<Canvas>().worldCamera == null)
+        {
+            //Set canvas event camera
+            keypadRef.GetComponentInChildren<Canvas>().worldCamera = GameObject.FindObjectsOfType<AgentPlayer>()[0].GetComponentInChildren<Camera>(); //NetManager.Instance.ClientPlayer.GetComponentInChildren<Camera>();
+        }
+    }
+}
