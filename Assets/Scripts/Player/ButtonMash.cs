39c3fc04007db3cf75212322da2f6ed8a6318c9f
Henry Banks <hbanks410@gmail.com>
2018-01-28 13:40:18 -0800

    Fixed out of range error with keypads. -Started ButtonMash script

---
+++ Assets/Scripts/Player/ButtonMash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMash : MonoBehaviour {
+
+    public string text;
+    public TextAsset file;
+    public int maxChunkLength = 10;
+    public int minChunkLength = 3;
+
+    List<string> chunks;
+
+	// Use this for initialization
+	void Start () {
+        text = file.text;
+
+        //Generate chunks
+        int i = 0;
+        while( i < text.Length)
+        {
+            int chunkLength = Random.Range(minChunkLength, maxChunkLength + 1);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
