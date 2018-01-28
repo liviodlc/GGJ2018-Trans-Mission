c0118026fa6f11cf321e365782cedce549333212
Henry Banks <hbanks410@gmail.com>
2018-01-28 03:42:28 -0800

    MotiveManager

---
+++ Assets/Scripts/Motives/MotiveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MotiveManager : Singleton<MotiveManager> {
+
+    public int agentMotivesComplete = 0;
+    public int doctorMotivesComplete = 0;
+    public GameObject player;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void MotiveComplete(bool isAgent)
+    {
+        if (isAgent)
+        {
+            agentMotivesComplete++;
+            //player.GetComponent<AgentPlayer>().motiveText.text = "AGENT MOTIVE COMPLETE!";
+
+        }
+        else
+        {
+            doctorMotivesComplete++;
+            //player.GetComponent<AgentPlayer>().motiveText.text = "DOCTOR MOTIVE COMPLETE!";
+        }
+
+       // player.GetComponent<AgentPlayer>().DoBlink();
+
+        //GameObject players[] = [NetManager.singleton.ClientPlayer, ]
+    }
+}
