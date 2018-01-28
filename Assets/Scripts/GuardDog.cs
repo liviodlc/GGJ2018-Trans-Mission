49e7e4937349fd23d76c92f668e7d3013b1da0d8
Clinton <clinton.reddie@live.com>
2018-01-28 15:20:09 -0800

    cleaning the dog

--- Assets/Scripts/GuardDog.cs
+++ Assets/Scripts/GuardDog.cs
@@ -20,9 +20,6 @@ public class GuardDog : MonoBehaviour, IObserverCallbacks
 
 	private NavMeshAgent ThisNavAgent;
 	private float OrigSpeed;
-	private float timer;
-	private bool IsLooking;
-	private Transform LookTarget;
 	private navMove PathComp;
 	private AudioSource ThisAudio;
 
@@ -32,21 +29,6 @@ public class GuardDog : MonoBehaviour, IObserverCallbacks
 		PathComp = GetComponent<navMove>();
 		ThisAudio = GetComponent<AudioSource>();
 		OrigSpeed = ThisNavAgent.speed;
-	}
-
-	private void Update()
-	{
-		if(IsLooking)
-		{
-			if(timer < WaitToLookTime)
-			{
-				timer += Time.deltaTime;
-				return;
-			}
-			Vector3 Targ = new Vector3(LookTarget.position.x, transform.position.y, LookTarget.position.z);
-			var TargetRot = Quaternion.LookRotation(Targ - transform.position);
-			transform.rotation = Quaternion.Slerp(transform.rotation, TargetRot, LookSpeed * Time.deltaTime);
-		}
 	}
 
 	public void StopAndGo(float WaitTime)
@@ -70,8 +52,6 @@ public class GuardDog : MonoBehaviour, IObserverCallbacks
 		ThisAnim.SetBool("IsRunning", false);
 		ThisAudio.Stop();
 		yield return new WaitForSeconds(WaitTime);
-		IsLooking = false;
-		timer = 0;
 		ThisAnim.SetBool("isWalking", true);
 		ThisNavAgent.speed = OrigSpeed;
 		PlaySound("Walking");
@@ -111,17 +91,10 @@ public class GuardDog : MonoBehaviour, IObserverCallbacks
 		ThisAudio.PlayOneShot(SniffSound);
 	}
 
-	public void LookAtSomething(Transform Target)
-	{
-		LookTarget = Target;
-		IsLooking = true;
-	}
-
 	public void OnDetectedTarget(SightTargetInfo info)
 	{
 		PathComp.enabled = false;
 		ThisNavAgent.speed = RunSpeed;
-		ThisNavAgent.SetDestination(info.target.transform.position);
 		ThisAnim.SetTrigger("Bark");
 		ThisAnim.SetBool("IsRunning", true);
 		PlaySound("Bark");
@@ -133,10 +106,11 @@ public class GuardDog : MonoBehaviour, IObserverCallbacks
 		ThisAudio.loop = false;
 	}
 
+	public void OnDetectingTarget(SightTargetInfo info) { ThisNavAgent.SetDestination(info.target.transform.position); }
+
 	public void OnTargetCameIntoRange(SightTargetInfo info) {}
 	public void OnTargetWentOutOfRange(SightTargetInfo info) {}
 	public void OnTargetDestroyed(SightTargetInfo info) {}
 	public void OnTryingToDetectTarget(SightTargetInfo info) {}
-	public void OnDetectingTarget(SightTargetInfo info) {}
 	public void OnStopDetectingTarget(SightTargetInfo info) {}
 }
